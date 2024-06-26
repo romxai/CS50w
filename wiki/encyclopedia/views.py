fd565eda892912c292184ed71e66a9de84eb3407
romxai <dwaynesilvapinto@gmail.com>
2024-06-26 19:19:57 +0530

    'update'

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
+def convert_markdown(title):
+    content = util.get_entry(title)
+    md = Markdown()
+    if content is None:
+        return None
+    else:
+        return md.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    content = convert_markdown(title)
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found."
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": content
+        })
